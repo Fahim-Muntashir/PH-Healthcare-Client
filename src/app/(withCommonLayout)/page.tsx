5a38b4031be04d6410149eaf126d0dce53be983f
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-05-08 21:46:33 +0600

    why us page done

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/Ui/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/Ui/HomePage/Specialist/Specialist";
 import TopRatedDoctors from "@/components/Ui/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/Ui/HomePage/WhyUs/WhyUs";
 import { Button } from "@mui/material";
 
 const HomePage = () => {
@@ -9,6 +10,7 @@ const HomePage = () => {
       <HeroSection />
       <Specialist></Specialist>
       <TopRatedDoctors></TopRatedDoctors>
+      <WhyUs></WhyUs>
     </>
   );
 };
