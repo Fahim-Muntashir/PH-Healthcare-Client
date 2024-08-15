a2d04d0041bee8404d56aae455174bd8e40f3486
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-08-15 10:57:45 +0600

    Bug gix import lazy loading today done

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,19 +1,17 @@
 'use client';
+import AuthButton from "@/components/Ui/AuthButton/AuthButton";
 import { getUserInfo, isLoggedIn, removeUser } from "@/services/auth.service";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-    const userInfo = getUserInfo();
 
-    const router = useRouter()
-    console.log(isLoggedIn());
-
-    const handleLogout = () => {
-        removeUser();
-        router.refresh()
-    }
+    const AuthButton = dynamic(() => import('@/components/Ui/AuthButton/AuthButton'),
+        {
+            ssr: false
+        })
 
     return (
         <Container>
@@ -30,11 +28,7 @@ const Navbar = () => {
                     <Typography component={Link} href="/login">Diagonostics</Typography>
                     <Typography component={Link} href="/login">NGOs</Typography>
                 </Stack>
-                {userInfo?.userId ?
-
-                    (< Button color="error" onClick={handleLogout}>Logout</Button>
-                    ) :
-                    (< Button component={Link} href="/login">Login</Button>)}
+                <AuthButton />
             </Stack>
         </Container >
     );
---
+++ src/components/Ui/AuthButton/AuthButton.tsx
@@ -0,0 +1,27 @@
+import { getUserInfo, isLoggedIn, removeUser } from "@/services/auth.service";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const AuthButton = () => {
+    const userInfo = getUserInfo();
+
+    const router = useRouter()
+
+    const handleLogout = () => {
+        removeUser();
+        router.refresh()
+    }
+
+    return (
+        <>
+            {userInfo?.userId ?
+
+                (< Button color="error" onClick={handleLogout}>Logout</Button>
+                ) :
+                (< Button component={Link} href="/login">Login</Button>)}
+        </>
+    );
+};
+
+export default AuthButton;
