fb571ddfb168f4f2d8834621305443ab5cd1aaa7
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-10-11 19:08:14 +0600

    add the appoinment page

---
+++ src/app/(withCommonLayout)/video/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VideoCallPage = ({ searchParams }: { searchParams: { videoCallingId: string } }) => {
+
+
+    const videoCallingId = searchParams.videoCallingId;
+
+    return (
+        <div>
+            asdfasdf
+        </div>
+    );
+};
+
+export default VideoCallPage;
